b89ffa47ffcef441e7350e76d2fa3ec531f68cdb
Prasoon-mishra <prasoonmishra987@gmail.com>
2022-08-16 11:17:41 +0530

    [Prasoon] Added employee present or absent program

---
+++ Employee_Wage_Computation/EmployeePresentOrAbesnt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Wage_Computation
+{
+    public class EmployeePresentOrAbesnt
+    {
+        public void PresentOrAbsent()
+        {
+            int Is_Full_Time = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is not present");
+            }
+
+        }
+
+    }
+}
---
+++ Employee_Wage_Computation/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Employee_Wage_Computation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage computation Program");
+            Console.WriteLine("Enter 1 to check whether employee present or not");
+            int ans = Convert.ToInt32(Console.ReadLine());
+            switch(ans)
+            {
+                case 1: 
+                    EmployeePresentOrAbesnt empPresntAbsent = new EmployeePresentOrAbesnt();
+                    empPresntAbsent.PresentOrAbsent();
+                    break;
+                default:
+                    Console.WriteLine("Entered value is incorrect");
+                    break;
+            }
+        }
+    }
+}
