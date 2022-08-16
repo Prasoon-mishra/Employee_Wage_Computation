31758eb9e05d8f8c1c34e915412e083133a91542
Prasoon-mishra <prasoonmishra987@gmail.com>
2022-08-16 11:25:27 +0530

    [Prasoon] Addedd Daily Salary of an employee

--- Employee_Wage_Computation/Program.cs
+++ Employee_Wage_Computation/Program.cs
@@ -6,12 +6,17 @@
         {
             Console.WriteLine("Welcome to employee wage computation Program");
             Console.WriteLine("Enter 1 to check whether employee present or not");
+            Console.WriteLine("Enter 2 to check the daily wage of an employee");
             int ans = Convert.ToInt32(Console.ReadLine());
             switch(ans)
             {
                 case 1: 
                     EmployeePresentOrAbesnt empPresntAbsent = new EmployeePresentOrAbesnt();
                     empPresntAbsent.PresentOrAbsent();
+                    break;
+                case 2:
+                    DailyWage empWage = new DailyWage();
+                    empWage.DailyWageCalculation();
                     break;
                 default:
                     Console.WriteLine("Entered value is incorrect");
